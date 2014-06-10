38e24d3cea20343bbc07beeaccf0e1fbf4bcc70b
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-06-10 16:06:44 +0200

    Re-initializing the project

--- project.clj
+++ project.clj
@@ -1,16 +1,8 @@
-(defproject io.screen6/cardinality "0.1.0-SNAPSHOT"
-  :description ""
+(defproject io.screen6/estimators "0.1.0-SNAPSHOT"
+  :description "Cardinality estimation API"
+  :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [alembic "0.2.1"]
-                                  [net.agkn/hll "1.5.1"]
-                                  [com.clearspring.analytics/stream "2.6.0"]
-                                  [midje "1.6.3"
-                                   :exclusions [org.clojure/clojure]]]
-                   :source-paths ["dev"]}}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [immutable-bitset "0.1.6"]
-                 [primitive-math "0.1.3"]
-                 [com.google.guava/guava "16.0.1"]
-                 [potemkin "0.3.4"]])
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
