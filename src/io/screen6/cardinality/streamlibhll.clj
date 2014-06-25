3621b05d95e8384d900bf13775e9fe4a05536444
Andrii Mishkovskyi <mishok13@lokai.local>
2014-06-25 15:48:28 +0200

    Small updates.

--- src/io/screen6/cardinality/streamlibhll.clj
+++ src/io/screen6/cardinality/streamlibhll.clj
@@ -6,16 +6,29 @@
    [com.clearspring.analytics.stream.cardinality HyperLogLog])
   (:refer-clojure :exclude [merge]))
 
-(defrecord Estimator [hll]
+(defrecord Estimator [hll precision]
 
   p/EstimatorProtocol
+
   (present [this value]
-    )
+    (assert hll "HyperLogLog instance is nil")
+    ;; Note that since HyperLogLog is a mutable object we have to rely
+    ;; on consumers of this API to be careful.
+    (.offer hll value)
+    this)
+
   (cardinality [this]
-    )
+    (assert hll "HyperLogLog instance is nil")
+    (.cardinality hll))
+
   (union [this other]
-    ))
+    (assert (instance? Estimator other) (format "Can't union with %s" other))
+    (assert (= (:precision this) (:precision other)) "Precision mismatch")
+    (assoc this :hll (doto (HyperLogLog. precision)
+                       (.addAll (:hll this))
+                       (.addAll (:hll other))))))
 
 (defn estimator
   "Create HyperLogLog estimator with a given precision"
-  [precision])
+  [precision]
+  (->Estimator (HyperLogLog. precision) precision))
