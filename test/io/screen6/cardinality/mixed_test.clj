4f8b729b07c7a7887450ee9368ea83d331eb1082
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-07-18 14:13:03 +0200

    Fixing tests (the cutoff was lowered a bit).

--- test/io/screen6/cardinality/mixed_test.clj
+++ test/io/screen6/cardinality/mixed_test.clj
@@ -12,8 +12,8 @@
     (:mode estimator) => ?mode))
  ?estimator ?values ?result ?error ?mode
  (estimator 12) nil 0 0.0 :set
- (estimator 12) (range 500) 500 0.0 :set
- (estimator 12) (range 502) 502 0.025 :hll
+ (estimator 12) (range 400) 400 0.0 :set
+ (estimator 12) (range 500) 500 0.025 :hll
  (estimator 12) (range 1000000) 1000000 0.025 :hll
  (estimator 15) (range 1000000) 1000000 0.025 :hll
  (estimator 12) (take 1000000 (cycle (range 10000))) 10000 0.05 :hll)
