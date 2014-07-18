faaf0cedbe6460b5e3467fbfa573500e86300d90
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-07-18 14:09:05 +0200

    Adding coverage support.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :url "https://github.com/screen6/cardinality"
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :dev {:plugins [[lein-midje "3.1.1"]]
+             :dev {:plugins [[lein-midje "3.1.1"]
+                             [lein-cloverage "1.0.2"]]
                    :dependencies [[midje "1.6.3"]
                                   [org.clojure/tools.namespace "0.2.5"]
                                   [com.taoensso/nippy "2.6.3"]]}}
