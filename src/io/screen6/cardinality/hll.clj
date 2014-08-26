741a7ab695510af917f7d9683ab6cd28abdc2945
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-08-26 10:55:42 +0200

    Empty simple HLL implementation

--- src/io/screen6/cardinality/hll.clj
+++ src/io/screen6/cardinality/hll.clj
@@ -3,13 +3,23 @@
   (:require
    [io.screen6.cardinality.estimator :as estimator]))
 
-(defrecord Estimator
-    []
+(defn- get-bits
+  [n start stop])
 
+(defn- hash32
+  [x])
+
+(defn- leading-zeros
+  [x])
+
+(defrecord Estimator [registers alpha m p]
   estimator/EstimatorProtocol
-
   (present [this value]
-    this)
+    (let [hash (hash32 value)
+          index (get-bits hash 0 p)
+          bits (get-bits hash p 32)]
+      (assoc-in this [:registers index]
+                (max (get-in this [:registers index] 0) (leading-zeros bits)))))
   (cardinality [this] 0)
   (union [this other] this))
 
